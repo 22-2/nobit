37b40f627969af460e4748c0eb05ca3ca20e39ca
22-2 <17890m@protonmail.ch>
2025-09-19 05:46:10 +0900

    add sourcemap

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,29 +1,36 @@
-import type { StorybookConfig } from '@storybook/svelte-vite';
+import type { StorybookConfig } from "@storybook/svelte-vite";
 
-import { join, dirname } from "path"
+import { join, dirname } from "path";
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+    return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|ts|svelte)"
-  ],
-  "addons": [
-    getAbsolutePath('@storybook/addon-svelte-csf'),
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest")
-  ],
-  "framework": {
-    "name": getAbsolutePath('@storybook/svelte-vite'),
-    "options": {}
-  }
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
+    addons: [
+        getAbsolutePath("@storybook/addon-svelte-csf"),
+        getAbsolutePath("@chromatic-com/storybook"),
+        getAbsolutePath("@storybook/addon-docs"),
+        getAbsolutePath("@storybook/addon-a11y"),
+        getAbsolutePath("@storybook/addon-vitest"),
+    ],
+    framework: {
+        name: getAbsolutePath("@storybook/svelte-vite"),
+        options: {},
+    },
+    async viteFinal(config) {
+        // ソースマップを有効にする
+        config.build = config.build || {};
+        config.build.sourcemap = true;
+
+        config.css = config.css || {};
+        config.css.devSourcemap = true;
+
+        return config;
+    },
 };
 export default config;
