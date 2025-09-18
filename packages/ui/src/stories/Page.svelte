67cf54adb9f5aacc725bf2784244d9a6c382e349
22-2 <17890m@protonmail.ch>
2025-09-19 04:24:04 +0900

    add obsidian-variables

--- packages/ui/src/stories/Page.svelte
+++ packages/ui/src/stories/Page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import "./page.css";
-    import "./obsidian-app.css";
+    import "./obsidian-variables.css";
     import Header from "./Header.svelte";
 
     let user = $state<{ name: string }>();
