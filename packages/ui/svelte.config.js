22528ac123227ea251a30234e99e53736e818920
22-2 <17890m@protonmail.ch>
2025-09-19 04:06:42 +0900

    config

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -1,13 +1,13 @@
-import { sveltePreprocess } from "svelte-preprocess";
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess({
-    css: "injected",
-    warningFilter: (warning) => !warning.code.startsWith("a11y"), // a11y-から始まる警告をすべて無視
-  }),
+    // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+    // for more information about preprocessors
+    preprocess: sveltePreprocess({
+        css: 'injected',
+        warningFilter: (warning) => !warning.code.startsWith('a11y'), // a11y-から始まる警告をすべて無視
+    }),
 };
 
 export default config;
